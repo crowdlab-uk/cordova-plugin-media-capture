efe50e868928dcb26dc8e73062b3a26d0d83613b
Thomas Lee <thomas.lee999@gmail.com>
2021-02-26 02:48:45 +0000

    Change switch camera icon to proper version (#4)
    Co-authored-by: Thomas Lee <thomas.lee@LON-MB034.local>

--- src/ios/VideoRecordingViewController.m
+++ src/ios/VideoRecordingViewController.m
@@ -13,7 +13,6 @@
 #import <AVFoundation/AVFoundation.h>
 #import <CoreMedia/CoreMedia.h>
 
-
 @implementation TimeView {}
 
 - (instancetype)init
@@ -23,17 +22,17 @@
     self.layer.cornerRadius = 4;
     [self setBackgroundColor:UIColor.blackColor];
     [self setAlpha:0.6];
-    
+
     self.timeLabel = [UILabel new];
     self.timeLabel.translatesAutoresizingMaskIntoConstraints = NO;
     [self.timeLabel setTextColor:UIColor.whiteColor];
     [self addSubview:self.timeLabel];
-    
+
     NSLayoutConstraint *timeLabelMarginTop = [NSLayoutConstraint constraintWithItem:self.safeAreaLayoutGuide attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.timeLabel attribute:NSLayoutAttributeTop multiplier:1.0 constant:-4];
     NSLayoutConstraint *timeLabelMarginLeft = [NSLayoutConstraint constraintWithItem:self.safeAreaLayoutGuide attribute:NSLayoutAttributeLeft relatedBy:NSLayoutRelationEqual toItem:self.timeLabel attribute:NSLayoutAttributeLeft multiplier:1.0 constant:-4];
     NSLayoutConstraint *timeLabelMarginBottom = [NSLayoutConstraint constraintWithItem:self.safeAreaLayoutGuide attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.timeLabel attribute:NSLayoutAttributeBottom multiplier:1.0 constant:4];
     NSLayoutConstraint *timeLabelMarginRight = [NSLayoutConstraint constraintWithItem:self.safeAreaLayoutGuide attribute:NSLayoutAttributeRight relatedBy:NSLayoutRelationEqual toItem:self.timeLabel attribute:NSLayoutAttributeRight multiplier:1.0 constant:4];
-    
+
     [timeLabelMarginTop setActive:YES];
     [timeLabelMarginLeft setActive:YES];
     [timeLabelMarginBottom setActive:YES];
@@ -46,9 +45,7 @@
   [self.timeLabel setText:text];
 }
 
-
 @end
-    
 
 @implementation VideoRecordingViewController {
   bool isCameraPositionBack;
@@ -56,16 +53,14 @@
 
 - (NSString *)stringFromTimeInterval:(NSTimeInterval)timeInterval {
   NSDate *endingDate = [NSDate date];
-   
+
   NSDate *startingDate = [endingDate dateByAddingTimeInterval:(-timeInterval)];
   NSCalendar *calendar = NSCalendar.currentCalendar;
-
-
   NSDateComponents *componentsNow = [calendar components:NSCalendarUnitMinute | NSCalendarUnitSecond fromDate:startingDate toDate:endingDate options:@{}];
-  
+
   NSInteger minutes = [componentsNow minute];
   NSInteger seconds = [componentsNow second];
-            
+
   return [NSString stringWithFormat:@"%ld:%02ld", (long)minutes, (long)seconds];
 }
 
@@ -75,82 +70,104 @@
 
 - (void)viewDidLayoutSubviews {
   [self.previewView.previewLayer setVideoGravity: kCAGravityResizeAspectFill];
-  
+
 }
 
 
 - (void)viewDidLoad {
   isCameraPositionBack = true;
   [self.view setBackgroundColor:UIColor.blackColor];
-  
+
   self.previewView = [[CKFPreviewView alloc] initWithFrame:self.view.frame];
   [self.previewView setTranslatesAutoresizingMaskIntoConstraints:NO];
+
   [self.previewView setSession:self.videoSession];
-  self.previewView.backgroundColor = UIColor.blueColor;
+  self.videoSession.delegate = self;
+
+  self.previewView.backgroundColor = UIColor.blackColor;
   [self.view addSubview:self.previewView];
-  
+
   NSLayoutConstraint *previewCenterXConstraint = [NSLayoutConstraint constraintWithItem:self.previewView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
   NSLayoutConstraint *previewCenterYConstraint = [NSLayoutConstraint constraintWithItem:self.previewView attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0];
   NSLayoutConstraint *previewMinimumWidthConstraint = [NSLayoutConstraint constraintWithItem:self.previewView attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationGreaterThanOrEqual toItem:self.view.safeAreaLayoutGuide attribute:NSLayoutAttributeWidth multiplier:1.0 constant:0];
-  
+
   CGFloat screenProportion = self.view.safeAreaLayoutGuide.layoutFrame.size.height / self.view.safeAreaLayoutGuide.layoutFrame.size.width;
-  
+
   NSLayoutConstraint *previewProportionConstraint = [NSLayoutConstraint constraintWithItem:self.previewView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeWidth multiplier:screenProportion constant:0];
-  
-  self.videoSession.delegate = self;
+
   self.captureButton = [RecordButton new];
   [self.captureButton setBackgroundColor:UIColor.redColor];
-  
+
   [self.captureButton setTranslatesAutoresizingMaskIntoConstraints:NO];
   [self.captureButton setEnabled:YES];
-  
+
   [self.view addSubview:self.captureButton];
   [self.view bringSubviewToFront:self.captureButton];
   [self.captureButton addTarget:self action: @selector(recordButtonPressed) forControlEvents: UIControlEventTouchUpInside];
-  
+
   NSLayoutConstraint *recordButtonBottomConstraint = [NSLayoutConstraint constraintWithItem:self.view.safeAreaLayoutGuide attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:self.captureButton attribute:NSLayoutAttributeBottom multiplier:1.0 constant:20];
   NSLayoutConstraint *recordButtonCenterConstraint = [NSLayoutConstraint constraintWithItem:self.captureButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
   NSLayoutConstraint *recordButtonWidthConstraint = [NSLayoutConstraint constraintWithItem:self.captureButton attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:44];
   NSLayoutConstraint *recordButtonHeightConstraint = [NSLayoutConstraint constraintWithItem:self.captureButton attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:nil attribute:NSLayoutAttributeNotAnAttribute multiplier:1.0 constant:44];
-  
+
   [recordButtonBottomConstraint setActive:YES];
   [recordButtonCenterConstraint setActive:YES];
   [recordButtonWidthConstraint setActive:YES];
   [recordButtonHeightConstraint setActive:YES];
-  
+
   [previewMinimumWidthConstraint setActive:YES];
   [previewProportionConstraint setActive:YES];
   [previewCenterXConstraint setActive:YES];
   [previewCenterYConstraint setActive:YES];
-  
+
   UIBarButtonItem *doneButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneButtonPressed)];
   [doneButton setStyle:UIBarButtonItemStyleDone];
   self.navigationItem.rightBarButtonItem = doneButton;
-  
-  UIBarButtonItem *switchButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemCamera target:self action:@selector(switchButtonPressed)];
-  
-  
+
+  UIButton *cameraIconView = [UIButton buttonWithType:UIButtonTypeCustom];
+  [cameraIconView setFrame:CGRectMake(0, 0, 900, 900)];
+
+  cameraIconView.translatesAutoresizingMaskIntoConstraints = NO;
+  cameraIconView.layer.frame = cameraIconView.bounds;
+
+  CALayer *parentLayer = [CALayer new];
+  parentLayer.backgroundColor = [[UIColor blueColor] CGColor];
+  [cameraIconView.layer addSublayer:parentLayer];
+
+  UIImage *switchImage = [UIImage imageNamed:@"camera-switch-crowdlab"];
+  switchImage = [switchImage imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+
+  UIImageView *imv = [[UIImageView alloc] initWithImage:switchImage];
+
+  UITapGestureRecognizer *tapGesture =
+         [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                 action:@selector(switchButtonPressed)];
+  [imv addGestureRecognizer:tapGesture];
+
+  [cameraIconView addSubview:imv];
+
+  imv.tintColor = UIColor.systemBlueColor;
+
+  UIBarButtonItem *switchButton = [[UIBarButtonItem alloc] initWithCustomView:imv];
+
   self.timeView = [TimeView new];
   self.timeView.translatesAutoresizingMaskIntoConstraints = NO;
 
   [self.view addSubview:self.timeView];
-  
+
   NSLayoutConstraint *timeLabelTopConstraint = [NSLayoutConstraint constraintWithItem:self.view.safeAreaLayoutGuide attribute:NSLayoutAttributeTop relatedBy:NSLayoutRelationEqual toItem:self.timeView attribute:NSLayoutAttributeTop multiplier:1.0 constant:-8];
   NSLayoutConstraint *timeLabelCenterConstraint = [NSLayoutConstraint constraintWithItem:self.timeView attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:self.view attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0];
-  
+
   [timeLabelTopConstraint setActive:YES];
   [timeLabelCenterConstraint setActive:YES];
-  
+
   self.navigationItem.leftBarButtonItem = switchButton;
-  
-  //  self.toolbarItems = @[barButton];
 }
 
 - (void)recordButtonPressed {
   NSLog(@"recordbutton: %d", self.captureButton.selected);
   bool isRecording = self.captureButton.selected;
-  
-  
+
   [self.captureButton setSelected:!isRecording];
   if (isRecording) {
     [self.delegate shouldEndRecording];
@@ -168,26 +185,26 @@
 - (void)onTick {
   NSLog(@"self.timer.fireDate: %@", self.timer.fireDate);
   NSLog(@"date: %@", [[NSDate date] dateByAddingTimeInterval:self.targetDurationInSeconds]);
-  
+
   NSTimeInterval timeRecorded = [[NSDate date] timeIntervalSinceDate: self.timerStartDate];
-  
+
   if (self.targetDurationInSeconds == 0) {
     [self.timeView setText:[self stringFromTimeInterval:timeRecorded]];
-  
+
   } else {
     [self.timeView setText:[NSString stringWithFormat:@"%@ / %@", [self stringFromTimeInterval:timeRecorded], [self stringFromTimeInterval:self.targetDurationInSeconds]]];
-    
+
     if ([self.timer.fireDate compare:[self.timerStartDate dateByAddingTimeInterval:self.targetDurationInSeconds]] == NSOrderedDescending) {
-      
+
       [self.delegate shouldEndRecording];
       [self.captureButton setSelected:false];
       [self.timer invalidate];
     }
   }
-  
 }
 
 - (void)switchButtonPressed {
+  NSLog(@"switch");
   [self.delegate shouldSwitchCameras];
 }
 
@@ -198,7 +215,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
   [self.videoSession start];
-  
+
   NSLog(@"duration: %f", self.targetDurationInSeconds);
   if (self.targetDurationInSeconds == 0) {
     [self.timeView setText:@"0:00"];
@@ -209,7 +226,7 @@
 
 - (void)viewDidAppear:(BOOL)animated {
   [super viewDidAppear:animated];
-  
+
 }
 
 - (void)viewDidDisappear:(BOOL)animated {
@@ -217,10 +234,7 @@
 }
 
 - (void)didChangeValueWithSession:(CKFSession * _Nonnull)session value:(id _Nonnull)value key:(NSString * _Nonnull)key {
-  //  if ([key equals:@"zoom" ]) {
-  //      self.zoomLabel.text = String(format: "%.1fx", value as! Double)
-  //  }
-}
 
+}
 
 @end
